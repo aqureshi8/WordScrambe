1a19dcd7fac8abbfa38bfa2a24908e2e69a69201
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-20 18:03:35 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ahsan Qureshi on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
